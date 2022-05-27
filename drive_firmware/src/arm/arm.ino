af9c6d56e7b2cc7b75b4b2f0b226814af174f44d
AriadnaHCTec <A01749161@itesm.mx>
2022-05-27 18:07:21 -0500

    reestructured

---
+++ drive_firmware/src/arm/arm.ino
@@ -0,0 +1,175 @@
+//actualizacion 14 mayo
+#include <Arduino.h>
+#include <Servo.h>
+#include <Adafruit_NeoPixel.h>
+
+#define pinLeftServo 9
+#define pinRightServo 6
+#define pinGripper 10
+#define pinCamara 5
+
+
+// Pin to the Status LED
+#define pinStatusLed 11
+#define NUMPIXELS 64 //Número de píxeles
+#define DELAYVAL 1 //timpo de espera en ms 
+Adafruit_NeoPixel pixels(NUMPIXELS, pinStatusLed, NEO_GRB + NEO_KHZ800);
+
+Servo leftServo; // the servo used to control a camera that can look around the robot
+Servo rightServo;
+Servo gripperServo;
+Servo camServo;
+int last_color = 0;
+long int start;
+long int now;
+int flag = 0;
+
+struct DATA {
+    float servo1;
+    float servo2;
+    float servo3;
+    float joint3;    
+    float movement;
+} received;
+
+void setup() {
+    Serial.begin(9600);
+    /*leftServo.attach(pinLeftServo);
+    rightServo.attach(pinRightServo);
+    gripperServo.attach(pinGripper);
+    camServo.attach(pinCamara);
+    gripperServo.writeMicroseconds(1500);
+    camServo.write(180);
+    leftServo.write(0);
+    rightServo.write(170);
+    pixels.begin(); // Inicializamos el objeto "pixeles"*/
+}
+
+void loop() {
+    /*pixels.clear(); // Apagamos todos los LEDs
+        if (flag == 0){
+      start = millis ();
+      flag = 1;
+    }
+    now = millis();
+    if (now - start > 2500){
+            float value  = float(analogRead(A7)*25.0/1023);
+      Serial.println("v" + String(value));
+      flag = 0;
+    }*/
+    
+    if (Serial.available() >= sizeof(uint8_t)) {
+        delayMicroseconds(10);
+        uint8_t cmd = (uint8_t) Serial.read();
+        // SERVO MOTOR COMMAND
+        switch(cmd){
+          case 0:
+            Serial.readBytes((char *) &received.joint3, sizeof(float));
+            Serial.print("joint3");
+          break;
+          case 1:
+            Serial.readBytes((char *) &received.servo1, sizeof(float));
+            Serial.print("servo1");
+          break;
+          case 2:
+            Serial.readBytes((char *) &received.servo2, sizeof(float));
+            Serial.print("servo2");
+          break;
+          case 3:
+            Serial.readBytes((char *) &received.servo3, sizeof(float));
+            Serial.print("servo3");
+          break;
+          case 4:
+            Serial.readBytes((char *) &received.movement, sizeof(float));
+            Serial.print("centrifuga");
+          break;
+        }
+	/*        if (cmd == 0) {
+            Serial.readBytes((char *) &received.joint3, sizeof(float));
+            Serial.print("joint3");
+            /*int left_angle=(int)received.servo;
+            int right_angle=170-(int)received.servo;
+            Serial.print("Left Servo: ");
+            Serial.print(left_angle);
+            Serial.print("Right Servo: ");
+            Serial.print(right_angle);
+            leftServo.write(left_angle);
+            rightServo.write(right_angle);*/
+        //}
+        
+        /*// STATUS COMMAND
+        else if (cmd == 2) {
+            Serial.readBytes((char *) &received.status, sizeof(float));
+            if ((int) received.status == 1){
+                last_color = 1;
+                Serial.print("Blue: Teleoperation (Manually driving)");
+                for(int i=0; i<NUMPIXELS; i++) {
+                    pixels.setPixelColor(i, pixels.Color(0, 0, 255));
+                    pixels.show();
+                    delay(DELAYVAL);
+                }
+            }
+            else if ((int) received.status == 2){
+                last_color = 2;
+                Serial.print("Red: Autonomous operation");
+                for(int i=0; i<NUMPIXELS; i++) {
+                    pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+                    pixels.show();
+                    delay(DELAYVAL);
+                }
+            }
+            else if ((int) received.status == 3){
+                Serial.print("# Flashing Green: Successful arrival at a post or passage through a gate");
+                for(int i=0; i<10; i++) {
+
+                    for(int i=0; i<NUMPIXELS; i++) {
+                    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+                    pixels.show();
+                    delay(DELAYVAL);
+                    }
+                   
+                    for(int i=0; i<NUMPIXELS; i++) {
+                    pixels.setPixelColor(i, pixels.Color(0, 255, 0));
+                    pixels.show();
+                    delay(DELAYVAL);
+                    }
+                    delay(100);
+                }
+              if (last_color==1){
+                for(int i=0; i<NUMPIXELS; i++) {
+                    pixels.setPixelColor(i, pixels.Color(0, 0, 255));
+                    pixels.show();
+                    delay(DELAYVAL);
+                }
+              }
+              else if (last_color==2){
+                 for(int i=0; i<NUMPIXELS; i++) {
+                    pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+                    pixels.show();
+                    delay(DELAYVAL);
+                }
+              }
+             
+            }
+        }
+
+
+        
+        else if (cmd == 3) {
+            Serial.readBytes((char *) &received.gripper, sizeof(float));
+            int gripper_angle=(int)received.gripper;
+            Serial.print("Gripper_Angle: ");
+            Serial.print(gripper_angle);
+            gripperServo.writeMicroseconds(gripper_angle);
+        }
+        
+        else if (cmd == 4){
+            Serial.readBytes((char *) &received.cam, sizeof(float));
+            int cam_angle=(int)received.cam;
+            Serial.print("Cam Angle: ");
+            Serial.print(cam_angle);
+            camServo.write(cam_angle);
+        }*/
+
+    }
+}
--- scripts/simple_arm_lab.py
+++ scripts/simple_arm_lab.py
@@ -24,6 +24,9 @@ def on_new_joint3(data):
 	serial_msg = cmd_byte_map['joint3'] + struct.pack("<f", data.data)
 	Serial.write(serial_msg)
 
+def on_new_movement(data):
+	serial_msg = cmd_byte_map['movement'] + struct.pack("<f", data.data)
+	Serial.write(serial_msg)
 
 rospy.init_node("simple_arm_lab", anonymous = True)
 
@@ -31,6 +34,7 @@ subscriber_joint3 = rospy.Subscriber("arm_lab/joint3", Int32, on_new_joint3)
 subscriber_servo1 = rospy.Subscriber("arm_lab/servo1", Int32, on_new_servo1)
 subscriber_servo2 = rospy.Subscriber("arm_lab/servo2", Int32, on_new_servo2)
 subscriber_servo3 = rospy.Subscriber("arm_lab/servo3", Int32, on_new_servo3)
+subscriber_joint3 = rospy.Subscriber("arm_lab/centrifuga", Int32, on_new_movement)
 
 baudrate = (9600)
 Serial = serial.Serial(baudrate=baudrate)
@@ -38,9 +42,10 @@ Serial.port = "/dev/ttyACM0"
 Serial.open()
 
 cmd_byte_map = {
-    'joint3': b"\x04",
+    'joint3': b"\x00",
     'servo1': b"\x01",
     'servo2': b"\x02",
-	'servo3': b"\x03"
+	'servo3': b"\x03",
+	'movement': b"\x03"
 }
 rospy.spin()
